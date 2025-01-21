568c4c2338eb0f0de4ad2836311469dee06b52a6
armenmerikyan <armenmerikyan@yahoo.com>
2025-01-21 15:49:43 -0800

    update

--- store/views.py
+++ store/views.py
@@ -150,11 +150,13 @@ from django.shortcuts import redirect
 
 
 from django.contrib.sites.shortcuts import get_current_site
-from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode
+from django.template.loader import render_to_string 
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
+ 
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.auth import get_user_model
 
 pokerGPT_version = "00.00.06"
 small_blind_size = 10
