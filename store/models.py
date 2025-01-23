b355e3680c3a60fdded2a68d3c464c75059c5527
armenmerikyan <armenmerikyan@yahoo.com>
2025-01-22 23:09:45 -0800

    update

--- store/models.py
+++ store/models.py
@@ -123,13 +123,6 @@ class Cart(models.Model):
     def __str__(self):
         return self.cart_id
 
-class CartProduct(models.Model):
-    id = models.AutoField(primary_key=True)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
-    added_at = models.DateTimeField(auto_now_add=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
 
 class APIData(models.Model):
     data = models.JSONField(null=True)  # Store JSON data
@@ -602,3 +595,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name  
+    
+
+class CartProduct(models.Model):
+    id = models.AutoField(primary_key=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    added_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
