9e1580ac76aa9fdc4c4bb61da5b533048883a8b4
armenmerikyan <armenmerikyan@yahoo.com>
2025-01-24 13:24:48 -0800

    update

--- website/urls.py
+++ website/urls.py
@@ -25,7 +25,9 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import PasswordResetConfirmView 
 
 urlpatterns = [  
-    path('admin/', admin.site.urls, name='admin_dashboard'), 
+    path('admin/', admin.site.urls), 
+    path('admin_panel/', admin.site.urls, name='admin_dashboard'),  # Custom URL name
+    
     path('', views.index, name='index'), 
     path('verify_signature/', views.verify_signature, name='verify_signature'),    
     path('game_create/', views.game_create, name='game_create'),  
