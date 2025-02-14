32461aaaf19c5e996488e8e196bf6167520cbf62
armenmerikyan <armenmerikyan@yahoo.com>
2025-02-14 07:18:51 -0800

    update

--- store/forms.py
+++ store/forms.py
@@ -84,7 +84,7 @@ class UserProfileUpdateForm(forms.ModelForm):
 class UserCreationForm(UserCreationForm): 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name']
+        fields = ['username', 'first_name', 'last_name']
  
 
 class TweetForm(forms.ModelForm):
