3423e0f4dd6a845869980fd909755d556e63535a
armenmerikyan <armenmerikyan@yahoo.com>
2025-02-14 08:41:43 -0800

    update

--- store/decorators.py
+++ store/decorators.py
@@ -14,4 +14,15 @@ def staff_required(view_func):
         return view_func(request, *args, **kwargs)
     return _wrapped_view
 
- 
+def add_profile_to_context(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        profile = WebsiteProfile.objects.order_by('-created_at').first()
+        if not profile:
+            profile = WebsiteProfile(name="add name", about_us="some info about us")
+        # Call the original view function and add the profile to its context
+        response = view_func(request, *args, **kwargs)
+        if isinstance(response, render):
+            response.context_data['profile'] = profile
+        return response
+    return wrapper
