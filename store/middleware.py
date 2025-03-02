c6e9473f0dd3835973c1a309558849876522782a
armenmerikyan <armenmerikyan@yahoo.com>
2025-03-02 09:07:06 -0800

    update

--- store/middleware.py
+++ store/middleware.py
@@ -1,26 +1,12 @@
-# middleware.py
-import re
+# store/middleware.py
 from django.conf import settings
 
-class DynamicSecurityMiddleware:
+class DynamicAllowedHostsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         host = request.get_host().split(':')[0]  # Remove port if present
-        if self.is_valid_host(host):
-            if host not in settings.ALLOWED_HOSTS:
-                settings.ALLOWED_HOSTS.append(host)
-            origin = request.headers.get('Origin')
-            if origin and self.is_valid_origin(origin):
-                if origin not in settings.CSRF_TRUSTED_ORIGINS:
-                    settings.CSRF_TRUSTED_ORIGINS.append(origin)
+        if host not in settings.ALLOWED_HOSTS:
+            settings.ALLOWED_HOSTS.append(host)
         return self.get_response(request)
-
-    def is_valid_host(self, host):
-        # Add your validation logic here
-        return re.match(r'^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', host) is not None
-
-    def is_valid_origin(self, origin):
-        # Add your validation logic here
-        return re.match(r'^https?://[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', origin) is not None
