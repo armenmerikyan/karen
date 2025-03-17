2c7c1c36e4f2ab0c00325bbd07ba59e9982165ca
armenmerikyan <armenmerikyan@yahoo.com>
2025-03-17 16:29:54 -0700

    update

--- website/settings.py
+++ website/settings.py
@@ -131,6 +131,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [ 
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',  # Forces JSON output
+    ],
 }
 from datetime import timedelta
 
