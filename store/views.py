0f88bd5fd03d8e5f69b35ea2e4e6bba13d7b17a0
armenmerikyan <armenmerikyan@yahoo.com>
2025-03-17 17:53:11 -0700

    update

--- store/views.py
+++ store/views.py
@@ -31,6 +31,8 @@ from rest_framework.generics import CreateAPIView, ListAPIView, CreateAPIView, R
 from rest_framework.response import Response
 from rest_framework import status
 
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
 import os
 import csv
 import io
@@ -172,8 +174,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
 from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response 
 from rest_framework.permissions import IsAdminUser
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
