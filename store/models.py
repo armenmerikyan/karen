5d4043049d7dbff7da833a4ab29c401cf1a46b6e
armenmerikyan <armenmerikyan@yahoo.com>
2025-03-19 10:53:20 -0700

    update

--- store/models.py
+++ store/models.py
@@ -580,7 +580,7 @@ class WebsiteProfile(models.Model):
 
     dockerhub_username = models.CharField(max_length=255, help_text="The DockerHub username.", blank=True, null=True)
     dockerhub_password = models.CharField(max_length=255, help_text="The DockerHub password. Store securely!", blank=True, null=True)
-
+ 
     sendgrid_email = models.CharField(max_length=255, help_text="Send Grid Email.", blank=True, null=True)
     sendgrid_key = models.CharField(max_length=255, help_text="Send Grid API Key", blank=True, null=True)
 
