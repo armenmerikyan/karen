914583c6374a428b7208cc8500f306a7d744b9e5
armenmerikyan <armenmerikyan@yahoo.com>
2025-03-20 10:56:51 -0700

    update

--- store/context_processors.py
+++
@@ -1,2 +0,0 @@
-def oauth_token_processor(request):
-    return {"MY_TOKEN": request.session.get("MY_TOKEN", "")}
--- website/settings.py
+++ website/settings.py
@@ -189,8 +189,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                "store.context_processors.oauth_token_processor",  # Add this
+                'django.contrib.messages.context_processors.messages', 
             ],
         },
     },
