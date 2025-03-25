7973f8301f2e07d28f9e6f10461adde61ae4618d
armenmerikyan <armenmerikyan@yahoo.com>
2025-03-25 10:41:45 -0700

    update

--- store/views.py
+++ store/views.py
@@ -35,8 +35,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-
-print("test")
+ 
 import os
 import csv
 import io
