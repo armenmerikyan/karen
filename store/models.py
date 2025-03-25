509700571a3c4758c4c2b2038c6e2640eb9d92ec
armenmerikyan <armenmerikyan@yahoo.com>
2025-03-25 15:42:17 -0700

    update

--- store/models.py
+++ store/models.py
@@ -745,6 +745,15 @@ class Product(models.Model):
 
     digital_file = models.FileField(upload_to='product_files/', null=True, blank=True)  # New field for digital files
 
+    # In Product model
+    user_character = models.OneToOneField('UserCharacter', on_delete=models.CASCADE, null=True, blank=True, related_name='product')
+    
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+    
     # Foreign keys for category, brand, and lifecycle stage
     #category = models.ForeignKey('Category', on_delete=models.CASCADE)
     #brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
