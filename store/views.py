5f1adf3c8744d765b689dadff8f03e7c5bce77ab
armenmerikyan <armenmerikyan@yahoo.com>
2025-03-26 14:28:49 -0700

    update

--- store/views.py
+++ store/views.py
@@ -4758,7 +4758,7 @@ def user_chatbot_response_private(request, character_id):
 
     tools = None
     if character.user == request.user or character.allow_memory_update:
-        print("Condition passed")
+        logger.info("Adding ADD_MEMORY tool to chat_kwargs")
         tools = [
             {
                 "type": "function",
